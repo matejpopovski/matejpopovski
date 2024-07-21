a8d4d01d07f7b064549358bbb8ff25d2306e9beb
Matej Popovski <116771786+matejpopovski@users.noreply.github.com>
2024-07-21 12:21:07 -0500

    Update fetch_chess_stats.py

--- fetch_chess_stats.py
+++ fetch_chess_stats.py
@@ -23,7 +23,7 @@ def format_stats(stats):
     bullet = stats.get("chess_bullet", {})
 
     markdown = f"""
-## ♞ Live Chess.com Stats for MatejPopovski:
+## ♞ Live Chess.com Stats for MatejPopovski
 
 ### Rapid
 - **Rating:** {rapid.get("last", {}).get("rating", "N/A")}
